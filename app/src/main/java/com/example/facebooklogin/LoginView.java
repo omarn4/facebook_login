01c0b28045de0df08d0380eaa3d0c54d97c297d9
Omar Esam <omaressamn1@gmail.com>
2023-12-15 08:11:56 +0200

    Added login_view with username and password fields

---
+++ app/src/main/java/com/example/facebooklogin/LoginView.java
@@ -0,0 +1,13 @@
+package com.example.facebooklogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_view);
+    }
+}
